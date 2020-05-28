cdcfb448a219c0c912abc8868c3c23139f9df023
Guilherme Erba <guilhermeerba@hotmail.com>
2020-05-27 22:01:18 -0300

    update show data form

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,14 +10,14 @@
 
       <section class="valor-hora">
         <label for="price-hour">Valor Hora:</label>
-        <input id="price-hour" type="text" v-model="priceHour">
+        <input id="price-hour" type="text" v-model="priceHour" />
       </section>
     </div>
 
     <div class="row">
       <main>
         <ul>
-          <li v-for="(valor, index) in arrayValores" :key="index">
+          <li v-for="(valor, index) in objFeature" :key="index">
             <p>Funcionalidade: {{valor.feature}}</p>
             <p>Horas de Desenvolvimento: {{valor.devHours}}</p>
             <p>Horas de Teste: {{valor.qaHours}}</p>
@@ -26,7 +26,7 @@
       </main>
 
       <aside>
-        <h2>Funcionalidades:{{functions}}</h2>
+        <h2>Funcionalidades:{{features}}</h2>
         <h2>Horas de Desenvolvimento:{{devHours}}</h2>
         <h2>Horas de Teste:{{qaHours}}</h2>
         <h2>Valor Total:{{priceHour}}</h2>
@@ -34,54 +34,78 @@
     </div>
 
     <div class="form">
-          <form>
-            <label for="functions">Funcionalidade:</label>
-            <input id="functions" type="text" v-model.lazy="functions">
+      <form v-for="i in input" :key="i" type="text" class="form-control" :id='"item"+i'>
+        <label for="features">Funcionalidade:</label>
+        <input id="features" type="text" v-model.lazy="features" />
 
-            <label for="dev-hours">Horas de Desenvolvimento:</label>
-            <input id="dev-hours" type="number" v-model.lazy.number="devHours" class="test">
+        <label for="dev-hours">Horas de Desenvolvimento:</label>
+        <input id="dev-hours" type="number" v-model.lazy.number="devHours" class="test" />
 
-            <label for="qa-hours">Horas de Teste:</label>
-            <input id="qa-hours" type="number" v-model.lazy.number="qaHours">
-          </form>
-          <button @click="teste">Inserir</button>
-        </div>
+        <label for="qa-hours">Horas de Teste:</label>
+        <input id="qa-hours" type="number" v-model.lazy.number="qaHours" />
+      </form>
+      <!--<form v-for="i in input" :key="i" type="text" class="form-control" :id='"item"+i'>
+        <label for="features">Funcionalidade:</label>
+        <input id="features" type="text" class="features" />
+
+        <label for="dev-hours">Horas de Desenvolvimento:</label>
+        <input id="dev-hours" type="number" class="dev-hours" />
+
+        <label for="qa-hours">Horas de Teste:</label>
+        <input id="qa-hours" type="number" class="qa-hours" />
+      </form>-->
+      
+      <button @click="insertData">Inserir</button>
+      <button @click="input++" type="button">Adicionar</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Main',
+  name: "Main",
   data() {
     return {
       priceHour: 0,
-      functions: '',
+      features: "",
       devHours: 0,
       qaHours: 0,
-      arrayValores: null
-    }
+      objFeature: null,
+      input: 1
+    };
   },
   methods: {
-    teste() {
-      this.arrayValores = [
+    insertData() {
+      this.objFeature = [
         {
-          'feature': this.functions,
-          'devHours': this.devHours,
-          'qaHours': this.qaHours
+          feature: this.features,
+          devHours: this.devHours,
+          qaHours: this.qaHours
         }
-      ]
-      return this.arrayValores;
+      ];
+      return this.objFeature;
+
+      /*const elFeatures = document.querySelectorAll('.features');
+      const elDevHours = document.querySelector('.dev-hours').value;
+      const elQAHours = document.querySelector('.qa-hours').value;
+      for (let i = 0; i < elFeatures.length; i++) {
+        this.objFeature = [
+          {
+            feature: elFeatures[i].value
+          }
+        ]
+      }*/
     },
     createJson() {
       const jsonExport = {
-        'feature': this.functions,
-        'devHours': parseFloat(this.devHours),
-        'testHours': parseFloat(this.qaHours)
-      }
+        feature: this.features,
+        devHours: parseFloat(this.devHours),
+        testHours: parseFloat(this.qaHours)
+      };
       console.log(jsonExport);
-    },
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -93,7 +117,7 @@ export default {
   flex-direction: row;
   width: 100%;
   justify-content: space-between;
-  
+
   main {
     min-width: 70%;
     background: #222f3e;
@@ -113,6 +137,6 @@ export default {
   form {
     display: flex;
     flex-direction: column;
-  } 
+  }
 }
 </style>
