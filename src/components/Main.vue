479c717c4ce0dae22e751836a2d00afc2cfce16b
Guilherme Erba <guilhermeerba@hotmail.com>
2020-06-06 17:55:15 -0300

    data organized into object

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,16 +2,16 @@
   <div class="main">
     <div class="row">
       <section class="actions">
-        <button class="btn btn-primary btn-lg tooltip" data-tooltip="Adicionar" @click="modalFeature = !modalFeature"><i class="icon icon-plus"></i></button>
-        <button class="btn btn-primary btn-lg tooltip" data-tooltip="Deletar" @click="activeDelete = !activeDelete"><i class="icon icon-delete"></i></button>
-        <button class="btn btn-primary btn-lg tooltip" data-tooltip="Importar" @click="modalImport = !modalImport"><i class="icon icon-upload"></i></button>
+        <button class="btn btn-primary btn-lg tooltip" data-tooltip="Adicionar" @click="actionStatus.modalFeature = !actionStatus.modalFeature"><i class="icon icon-plus"></i></button>
+        <button class="btn btn-primary btn-lg tooltip" data-tooltip="Deletar" @click="actionStatus.activeDelete = !actionStatus.activeDelete"><i class="icon icon-delete"></i></button>
+        <button class="btn btn-primary btn-lg tooltip" data-tooltip="Importar" @click="actionStatus.modalImport = !actionStatus.modalImport"><i class="icon icon-upload"></i></button>
         <button class="btn btn-primary btn-lg tooltip" data-tooltip="Exportar" @click="exportJson"><i class="icon icon-download"></i></button>
       </section>
 
       <section class="valor-hora">
         <div class="input-group">
           <span class="input-group-addon">R$</span>
-          <input type="text" class="form-input" placeholder="Valor Hora" v-model.number.lazy="priceHour">
+          <input type="text" class="form-input" placeholder="Valor Hora" v-model.number.lazy="inputData.priceHour">
         </div>
       </section>
     </div>
@@ -20,10 +20,10 @@
       <main>
         <h2>Suas Features</h2>
 
-        <ul v-for="(item, index) in todos" :key="index">
+        <ul v-for="(item, index) in featureData" :key="index">
           <li>
             <strong>Funcionalidade:</strong> {{item.feature}}
-            <button class="btn btn-action s-circle btn-sm" data="delete-item" :class="activeDelete ? 'active' : ''" @click="deleteItem(index)"><i class="icon icon-cross"></i></button>
+            <button class="btn btn-action s-circle btn-sm" data="delete-item" :class="actionStatus.activeDelete ? 'active' : ''" @click="deleteItem(index)"><i class="icon icon-cross"></i></button>
           </li>
           <li><strong>Horas de Desenvolvimento:</strong> {{item.devHours}}h</li>
           <li><strong>Horas de Teste:</strong> {{item.qaHours}}h</li>
@@ -33,54 +33,43 @@
         <!--<ul v-for="(item, index) in importFeatures" :key="`${index}-${item.id}`">
           <li>
             <strong>Funcionalidade:</strong> {{item.feature}}
-            <button class="btn btn-action s-circle btn-sm" data="delete-item" :class="activeDelete ? 'active' : ''" @click="deleteItem(index)"><i class="icon icon-cross"></i></button>
+            <button class="btn btn-action s-circle btn-sm" data="delete-item" :class="actionStatus.activeDelete ? 'active' : ''" @click="deleteItem(index)"><i class="icon icon-cross"></i></button>
           </li>
-          <li><strong>Horas de Desenvolvimento:</strong> {{item.devHours}}h</li>
-          <li><strong>Horas de Teste:</strong> {{item.qaHours}}h</li>
-          <li><strong>Valor:</strong> {{item.pricePerFeature | numeroPreco}}</li>
-        </ul>-->
-
-        <!--<h1>---------TESTE---------</h1>
-        <ul v-for="(item, index) in todos" :key="index">
-          <li>
-            <strong>Funcionalidade:</strong> {{item.feature}}
-            <button class="btn btn-action s-circle btn-sm" data="delete-item" :class="activeDelete ? 'active' : ''" @click="deleteItem(index)"><i class="icon icon-cross"></i></button>
-          </li>
-          <li><strong>Horas de Desenvolvimento:</strong> {{item.devHours}}h</li>
+          <li><strong>Horas de Desenvolvimento:</strong> {{item.inputData.devHours}}h</li>
           <li><strong>Horas de Teste:</strong> {{item.qaHours}}h</li>
           <li><strong>Valor:</strong> {{item.pricePerFeature | numeroPreco}}</li>
         </ul>-->
       </main>
 
       <aside>
-        <h6>Total de Funcionalidades: {{features.length}}</h6>
+        <h6>Total de Funcionalidades: {{featureCounter.length}}</h6>
         <!--<ul>
           <li v-for="(feature, index) in features" :key="index">{{feature}}</li>
         </ul>-->
-        <h6>Total Horas de Desenvolvimento: {{totalDevHours}}</h6>
-        <h6>Total Horas de Teste: {{totalQaHours}}</h6>
-        <h6>Valor Total: {{totalPriceHour | numeroPreco}}</h6>
+        <h6>Total Horas de Desenvolvimento: {{sumTotal.totalDevHours}}</h6>
+        <h6>Total Horas de Teste: {{sumTotal.totalQaHours}}</h6>
+        <h6>Valor Total: {{sumTotal.totalPriceHour | numeroPreco}}</h6>
       </aside>
     </div>
 
-    <div v-if="modalFeature" class="modal" :class="modalFeature ? 'active' : ''" id="modal-id">
-      <a @click.prevent="modalFeature = !modalFeature" href="#close" class="modal-overlay" aria-label="Close"></a>
+    <div v-if="actionStatus.modalFeature" class="modal" :class="actionStatus.modalFeature ? 'active' : ''" id="modal-id">
+      <a @click.prevent="actionStatus.modalFeature = !actionStatus.modalFeature" href="#close" class="modal-overlay" aria-label="Close"></a>
       <div class="modal-container">
         <div class="modal-header">
-          <a @click.prevent="modalFeature = !modalFeature" href="#close" class="btn btn-clear float-right" aria-label="Close"></a>
+          <a @click.prevent="actionStatus.modalFeature = !actionStatus.modalFeature" href="#close" class="btn btn-clear float-right" aria-label="Close"></a>
           <div class="modal-title h5">Insira Suas Features</div>
         </div>
         <div class="modal-body">
           <div class="content">
             <form @submit="addFeatures">
               <label class="form-label" for="features">Funcionalidade:</label>
-              <input class="form-input" id="features" type="text" v-model.lazy="feature" />
+              <input class="form-input" id="features" type="text" v-model.lazy="inputData.featureName" />
 
               <label class="form-label" for="dev-hours">Horas de Desenvolvimento:</label>
-              <input class="form-input" id="dev-hours" type="number" v-model.number="devHours" />
+              <input class="form-input" id="dev-hours" type="number" v-model.number="inputData.devHours" />
 
               <label class="form-label" for="qa-hours">Horas de Teste:</label>
-              <input class="form-input" id="qa-hours" type="number" v-model.number="qaHours" />
+              <input class="form-input" id="qa-hours" type="number" v-model.number="inputData.qaHours" />
               <div class="modal-footer">
                 <button class="btn btn-primary btn-lg"><i class="icon icon-check"></i></button>
               </div>
@@ -90,11 +79,11 @@
       </div>
     </div>
 
-    <div v-if="modalImport" class="modal" :class="modalImport ? 'active' : ''" id="modal-id">
-      <a @click.prevent="modalImport = !modalImport" href="#close" class="modal-overlay" aria-label="Close"></a>
+    <div v-if="actionStatus.modalImport" class="modal" :class="actionStatus.modalImport ? 'active' : ''" id="modal-id">
+      <a @click.prevent="actionStatus.modalImport = !actionStatus.modalImport" href="#close" class="modal-overlay" aria-label="Close"></a>
       <div class="modal-container">
         <div class="modal-header">
-          <a @click.prevent="modalImport = !modalImport" href="#close" class="btn btn-clear float-right" aria-label="Close"></a>
+          <a @click.prevent="actionStatus.modalImport = !actionStatus.modalImport" href="#close" class="btn btn-clear float-right" aria-label="Close"></a>
           <div class="modal-title h5">Importe Seu JSON</div>
         </div>
         <div class="modal-body">
@@ -113,7 +102,7 @@ export default {
   name: "Main",
   data() {
     return {
-      /*todos: [
+      /*featureData: [
         {
         'id': 1,
         'feature': 'Correção BUG',
@@ -121,21 +110,24 @@ export default {
         'qaHours': 1.2
         }
       ],*/
-      modalFeature: false,
-      modalImport: false,
-      activeDelete: false,
-      todos: [],
-      feature: "",
-      devHours: "",
-      qaHours: "",
-      priceHour: "",
-      totalDevHours: "",
-      totalQaHours: "",
-      totalPriceHour: "",
-      listPriceHour: [],
-      features: [],
-      importFeatures: [],
-      teste: []
+      actionStatus: {
+        modalFeature: false,
+        modalImport: false,
+        activeDelete: false,
+      },
+      inputData: {
+        featureName: "",
+        devHours: "",
+        qaHours: "",
+        priceHour: "",
+      },
+      sumTotal: {
+        totalDevHours: "",
+        totalQaHours: "",
+        totalPriceHour: "",
+      },
+      featureData: [],
+      featureCounter: [],
     };
   },
   methods: {
@@ -143,18 +135,18 @@ export default {
       e.preventDefault();
       // Inserindo os Dados do Form em um Objeto
       let obj;
-      if (this.feature.trim().length && this.priceHour) {
+      if (this.inputData.featureName.trim().length && this.inputData.priceHour) {
         obj = {
-          //id: this.todos.length + 1,
-          feature: this.feature,
-          devHours: this.devHours,
-          qaHours: this.qaHours,
-          pricePerFeature: this.priceHour * (this.devHours + this.qaHours)
+          //id: this.featureData.length + 1,
+          feature: this.inputData.featureName,
+          devHours: this.inputData.devHours,
+          qaHours: this.inputData.qaHours,
+          pricePerFeature: this.inputData.priceHour * (this.inputData.devHours + this.inputData.qaHours)
         };
-        this.todos.push(obj);
-        this.feature = "";
-        this.devHours = "";
-        this.qaHours = "";
+        this.featureData.push(obj);
+        this.inputData.featureName = "";
+        this.inputData.devHours = "";
+        this.inputData.qaHours = "";
         this.pricePerFeature = "";
       
       // Soma das Horas de DEV, QA e Contador de Features
@@ -162,16 +154,16 @@ export default {
       let sumQAHours = 0;
       let sumFeatures = 0;
       let totalPriceHour = 0;
-      for (let i = 0; i < this.todos.length; i++) {
-        sumFeatures = this.todos[i].feature;
-        sumDevHours += this.todos[i].devHours;
-        sumQAHours += this.todos[i].qaHours;
-        this.totalPriceHour = (totalPriceHour += this.todos[i].pricePerFeature);
+      for (let i = 0; i < this.featureData.length; i++) {
+        sumFeatures = this.featureData[i].feature;
+        sumDevHours += this.featureData[i].devHours;
+        sumQAHours += this.featureData[i].qaHours;
+        this.sumTotal.totalPriceHour = (totalPriceHour += this.featureData[i].pricePerFeature);
       }
-      this.features.push(sumFeatures);
-      this.totalDevHours = sumDevHours;
-      this.totalQaHours = sumQAHours;
-      
+      this.featureCounter.push(sumFeatures);
+      this.sumTotal.totalDevHours = sumDevHours;
+      this.sumTotal.totalQaHours = sumQAHours;
+
       } else {
         alert('Preencha seu valor por hora!')
       }
@@ -182,21 +174,21 @@ export default {
         totalPriceHour += this.listPriceHour[i];
         console.log(this.listPriceHour[i])
       }
-      this.totalPriceHour = totalPriceHour;*/
+      this.sumTotal.totalPriceHour = totalPriceHour;*/
     },
     deleteItem(index) {
-      if(this.todos.length) {
-        this.$delete(this.todos, index);
+      if(this.featureData.length) {
+        this.$delete(this.featureData, index);
       }
-      if(this.importFeatures.length) {
+      /*if(this.importFeatures.length) {
         this.$delete(this.importFeatures, index);
-      }
+      }*/
     },
     exportJson() {
-      let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.todos));
+      let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.featureData));
       let downloadAnchorNode = document.createElement("a");
-      console.log(this.todos)
-      if(this.todos.length) {
+      console.log(this.featureData)
+      if(this.featureData.length) {
         downloadAnchorNode.setAttribute("href", dataStr);
         downloadAnchorNode.setAttribute("download", "features" + ".json");
         document.body.appendChild(downloadAnchorNode);
@@ -216,15 +208,15 @@ export default {
       reader.readAsText(file, "UTF-8");
       reader.onload = evt => {
         //this.text = evt.target.result;
-        this.todos = [];
-        this.features = [];
+        this.featureData = [];
+        this.featureCounter = [];
         
         let result = JSON.parse(evt.target.result);
         let obj;
         let sumDevHours = 0;
         let sumQAHours = 0;
         let totalPriceHour = 0;
-        if(this.priceHour) {
+        if(this.inputData.priceHour) {
           for (let i = 0; i < result.length; i++) {
             // Inserindo os Dados do Json em um Objeto
             obj = {
@@ -232,32 +224,32 @@ export default {
               feature: result[i].feature,
               devHours: result[i].devHours,
               qaHours: result[i].qaHours,
-              pricePerFeature: this.priceHour * (result[i].devHours + result[i].qaHours)
+              pricePerFeature: this.inputData.priceHour * (result[i].devHours + result[i].qaHours)
             };
-            this.todos.push(obj);
+            this.featureData.push(obj);
 
             // Soma das Horas de DEV, QA e Contador de Features
-            this.features.push(result[i].feature);
-            this.totalDevHours = (sumDevHours += result[i].devHours);
-            this.totalQaHours = (sumQAHours += result[i].qaHours);
+            this.featureCounter.push(result[i].feature);
+            this.sumTotal.totalDevHours = (sumDevHours += result[i].devHours);
+            this.sumTotal.totalQaHours = (sumQAHours += result[i].qaHours);
 
             // Soma de Todos os Valores
-            this.totalPriceHour = (totalPriceHour += this.todos[i].pricePerFeature);
+            this.sumTotal.totalPriceHour = (totalPriceHour += this.featureData[i].pricePerFeature);
           }
         } else {
           alert('Preencha seu valor por hora!');
-          this.modalImport = false;
+          this.actionStatus.modalImport = false;
         }
       };
       reader.onerror = evt => {
         console.error(evt);
       };
-      this.modalImport = false;
+      this.actionStatus.modalImport = false;
     }
   },
   beforeUpdate() {
-    //this.totalPriceHour = this.priceHour * (this.totalDevHours + this.totalQaHours);
-    //console.log(`Price Hour ${this.priceHour}`);
+    //this.sumTotal.totalPriceHour = this.inputData.priceHour * (this.sumTotal.totalDevHours + this.sumTotal.totalQaHours);
+    //console.log(`Price Hour ${this.inputData.priceHour}`);
   }
 };
 </script>
